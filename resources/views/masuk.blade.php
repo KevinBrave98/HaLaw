de51cbb5acef5f03432e55563534a883a04e8738
Ashahra Aprilia Puspaanggraini <ashahraaprilia@gmail.com>
2025-06-03 16:18:40 +0700

    update halaman masuk

---
+++ resources/views/masuk.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="masuk.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    @vite(['resources/js/app.js', 'resources/sass/app.scss'])
+    <link rel="stylesheet" href="assets/styles/masuk.css">
+    <title>Halaman Masuk</title>
+</head>
+<body>
+    <div class="kolom">
+        {{-- <div class="form-masuk">
+            
+        </div>
+        <div class="foto">
+            
+        </div> --}}
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Route::get('/tes', function () {
     return view('coba');
 });
+
+Route::get('/masuk', function () {
+    return view('masuk');
+});
