f32e0c1b054acd838f400c4402dafc20095850f0
helenaviveca <hhelenaviveca@gmail.com>
2025-06-04 14:03:54 +0700

    fixed web.php

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,13 @@ Route::get('/footer', function () {
 
 Route::get('/user_register', function () {
     return view('user_register');
+});
+
+Route::get('/lawyer_register', function () {
+    return view('lawyer_register');
+
+});
+
 Route::get('/daftar', function () {
     return view('daftar');
 });
@@ -30,4 +37,23 @@ Route::get('/ubahprofilpengguna', function () {
 
 Route::get('/masuk', function () {
     return view('masuk');
+});
+
+Route::get('/dashboard_sebelum_login', function () {
+    return view('dashboard_sebelum_login');
+});
+
+Route::get('/navbar_sebelum_login', function () {
+    return view('navbar_sebelum_login');
+});
+Route::get('/pengacara_register', function () {
+    return view('pengacara_register');
+});
+
+Route::get('/dashboard_user', function () {
+    return view('dashboard_user');
+});
+
+Route::get('/navbar_user', function () {
+    return view('navbar_user');
 });
