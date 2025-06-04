a5abbf068012a7f1090fb0e7822c3ba0f62abeb8
nirvanaaputri <nirvanaaputri@gmail.com>
2025-06-04 15:07:50 +0700

    add controller for User Profile and Register
    menambahkan controller dan route untuk profil pengguna dan daftar

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,26 +16,21 @@ Route::get('/footer', function () {
     return view('footer');
 });
 
-Route::get('/user_register', function () {
-    return view('user_register');
-});
+Route::get('/daftar/pengguna', [RegisterController::class, 'showUser'])->name('userregis.show');
+Route::get('/daftar/pengacara', [RegisterController::class, 'showLawyer'])->name('lawyerregis.show');
 
-Route::get('/lawyer_register', function () {
-    return view('lawyer_register');
+// Route::get('/lawyer_register', function () {
+//     return view('lawyer_register');
 
-});
+// });
 
 Route::get('/daftar', function () {
     return view('daftar');
 });
 
-Route::get('/profilpengguna', function () {
-    return view('profil_pengguna');
-});
 
-Route::get('/ubahprofilpengguna', function () {
-    return view('ubah_profil_pengguna');
-});
+Route::get('/profil', [UserProfileController::class, 'show'])->name('profile.show');
+Route::get('/profil/ubah', [UserProfileController::class, 'edit'])->name('profile.edit');
 
 Route::get('/masuk', function () {
     return view('masuk');
@@ -51,11 +48,8 @@ Route::get('/footer', function () {
     return view('footer');
 });
 
-Route::get('/dashboard_pengguna_dan_pengacara_sebelum_login', function () {
-    return view('dashboard_pengguna_dan_pengacara_sebelum_login');
-});
-Route::get('/pengacara_register', function () {
-    return view('pengacara_register');
+Route::get('/dashboard_sebelum_login', function () {
+    return view('dashboard_sebelum_login');
 });
 
 Route::get('/dashboard_user', function () {
