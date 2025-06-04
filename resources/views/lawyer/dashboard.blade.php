4674f1916bfb1080015283a298d5f6399c7a9974
Ashahra Aprilia Puspaanggraini <ashahraaprilia@gmail.com>
2025-06-04 16:18:51 +0700

    added some styling

--- resources/views/lawyer/dashboard.blade.php
+++ resources/views/lawyer/dashboard.blade.php
@@ -4,21 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="masuk.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     @vite(['resources/js/app.js', 'resources/sass/app.scss'])
-    <link rel="stylesheet" href="assets/styles/lawyer_dashboard.css">
+    <link rel="stylesheet" href={{ asset('assets/styles/lawyer_dashboard.css') }}>
     <title>Dashboard Pengacara</title>
 </head>
 <body>
     <div class="container ">
-        <h1>Halo</h1>
-        {{-- <div class="greetings">
+        <div class="greetings">
             <h1>Halo</h1><a href="">{{ $nama_pengacara }}</a>
-        </div> --}}
-        {{-- <div class="consult-container">
+        </div>
+        <div class="consult-container">
             <h2>Cek Sesi Konsultasi yang Sedang Berjalan</h2>
             <button type="button" class="btn btn-primary btn-lg">Large button</button>
         </div>
@@ -70,7 +68,7 @@
                     </div>
                 </div>
             </div>
-        </div> --}}
+        </div>
     </div>
 </body>
 </html>
