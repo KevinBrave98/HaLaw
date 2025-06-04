064593dc29cb9dd4f7cccca7630a160477879543
KevinBrave98 <kevinbravewq@gmail.com>
2025-06-04 15:17:27 +0700

    fixed merge

--- public/assets/scripts/search_pengguna.js
+++
@@ -1,6 +0,0 @@
-let filter_icon = document.querySelector('#filter-icon')
-let more_filter = document.querySelector('.more-filter');
-
-filter_icon.addEventListener('click', () => {
-    more_filter.classList.toggle('hide')
-})
