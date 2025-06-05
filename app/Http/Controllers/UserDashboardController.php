2ec4ae088f971fff2baf625e1fa1de2bef2df311
Ronald.Cahyadi <ronal.cahyadi55@gmail.com>
2025-06-05 15:14:48 +0700

    update forntend dashboard user
    memperbaiki hover button telusuri istilah hukum

--- app/Http/Controllers/UserDashboardController.php
+++ app/Http/Controllers/UserDashboardController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 use App\Http\Controllers\Pengguna;
 use Illuminate\Http\Request;
+use App\Http\Controllers\pengacara;
+use Ramsey\Uuid\Type\Integer;
 
 class UserDashboardController extends Controller
 {
     public function greetings(string $nama_pengguna){
         return view('dashboard_user')->with('nama_pengguna', $nama_pengguna);
     }
+
+//     public function pengacara(string $nama_pengacara, string $spesialisasi, int $tarif_jasa){
+//         return view('dashboard_user')->with('nama_pengacara',)
+//     }
 }
