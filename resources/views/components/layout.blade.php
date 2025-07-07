ef2c1d21b91d69ac6dac4485aa091fa6fbabc8f7
KevinBrave98 <kevinbravewq@gmail.com>
2025-07-07 08:22:47 +0700

    fixed css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,6 @@
     <title>{{ $title }}</title>
     @vite(['resources/js/app.js', 'resources/sass/app.scss'])
     @stack('css')
-    <link rel="stylesheet" href="{{ asset('assets/styles/lawyer_dashboard.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/styles/navbar_sebelum_login.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/styles/footer.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
