7526a80b45e6bbd7c7a8635675c427a1327efc5a
Ronald.Cahyadi <ronal.cahyadi55@gmail.com>
2025-07-17 08:21:44 +0700

    mau rebase update dashboard user

--- resources/views/user/dashboard_user.blade.php
+++ resources/views/user/dashboard_user.blade.php
@@ -1,6 +1,7 @@
 @push('css')
     <link rel="stylesheet" href="{{ asset('assets/styles/dashboard_user.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/styles/search_pengacara.css') }}">
+    <link rel="stylesheet" href="">
 @endpush
 <x-layout_user :title="'Halaw - Dashboard User'">
     <div class="content">
@@ -9,6 +10,39 @@
         <div class="scroll-wrapper">
             <div class="scroll-container">
                 @foreach ($pengacara as $lawyer_card)
+                    {{-- <div class="badges">
+                        @if ($lawyer_card->chat)
+                            <span class="badge-custom">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                    fill="currentColor" class="bi bi-chat-fill" viewBox="0 0 16 16">
+                                    <path d="M8 0a8 8 0 0 0-6.84 12.29L0 16l3.71-1.16A8 8 0 1 0 8 0z" />
+                                </svg>
+                                Pesan
+                            </span>
+                        @endif
+
+                        @if ($lawyer_card->voice_chat)
+                            <span class="badge-custom">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                    fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
+                                </svg>
+                                Panggilan suara
+                            </span>
+                        @endif
+
+                        @if ($lawyer_card->video_call)
+                            <span class="badge-custom">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                    fill="currentColor" class="bi bi-camera-video-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="M0 5a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v.5l3.5-2v9l-3.5-2V11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5z" />
+                                </svg>
+                                Panggilan Video
+                            </span>
+                        @endif
+                    </div> --}}
                     <div class="lawyer-card">
                         @if ($lawyer_card->foto_pengacara == null)
                             <div class="image_wrapper">
