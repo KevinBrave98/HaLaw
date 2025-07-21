e77525420217684c7e0787bdc6de3f8d57520cc3
helenaviveca <hhelenaviveca@gmail.com>
2025-07-21 15:49:13 +0700

    fixed filter from dashboard

--- app/Http/Controllers/UserDashboardController.php
+++ app/Http/Controllers/UserDashboardController.php
@@ -15,6 +15,16 @@ class UserDashboardController extends Controller
         $lawyers = Pengacara::where('status_konsultasi', 1);
         $harga_max = $lawyers->max('tarif_jasa');
         $harga_min = $lawyers->min('tarif_jasa');
+
+        if($harga_max % 1000 != 0) {
+            $harga_max += 1000 - ($harga_max % 1000);
+            // dd($harga_max);
+        }
+
+        if($harga_min % 1000 != 0) {
+            $harga_min -= 1000 + ($harga_min % 1000);
+        }
+
         $pengacara = DB::table('pengacaras')->inRandomOrder()->limit(5)->get();
         $pengguna = Auth::user();
         return view('user.dashboard_user', compact('pengacara', 'pengguna', 'harga_max', 'harga_min'));
