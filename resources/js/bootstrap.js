07a868a2eae3ce99ec267a8f3c3d4fff8800526d
KevinBrave98 <kevinbravewq@gmail.com>
2025-07-23 11:02:12 +0700

    added procfile and some scripts, also updated wsport, and  vite

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -25,10 +25,10 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb', // or pusher
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: window.location.hostname,
-    wsPort: 8080,
-    wssPort: 8080,
-    forceTLS: false,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
     authEndpoint: '/broadcasting/auth',
     withCredentials: true,
