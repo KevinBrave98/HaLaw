ac4406a8b78505c22e79aaaaa5e5516d46eae178
KevinBrave98 <kevinbravewq@gmail.com>
2025-07-23 15:07:55 +0700

    updated code, trying to fix chat

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -25,10 +25,10 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb', // or pusher
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    wsHost: halaw-reverb-production.up.railway.app,
+    wsPort: 80,
+    wssPort: 443,
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
     authEndpoint: '/broadcasting/auth',
     withCredentials: true,
