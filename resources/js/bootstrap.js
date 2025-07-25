e239baaa7e5c5917ed7e11fda4ef3f15b2da8eee
KevinBrave98 <kevinbravewq@gmail.com>
2025-07-25 23:33:50 +0700

    FIXED THIS THING FOR DEV

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -25,10 +25,10 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb', // or pusher
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: 'halaw-reverb-production.up.railway.app',
-    wsPort: 80,
-    wssPort: 443,
-    forceTLS: true,
+    wsHost: window.location.hostname,
+    wsPort: 8080,
+    wssPort: 8080,
+    forceTLS: false,
     enabledTransports: ['ws', 'wss'],
     authEndpoint: '/broadcasting/auth',
     withCredentials: true,
